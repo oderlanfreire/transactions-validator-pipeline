49bd2c5e4d6db0f1e59cdd05a813367971904da9
Oderlan Freire <oderlanfreire@gmail.com>
2025-12-03 15:41:42 -0300

    settings: ajuste no nome do arquivo script(old) para transaction_pipeline_main(new). dev: inicio do desenvolvimento do script sh

--- run_transac_validation.sh
+++ run_transac_validation.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+BLUE="\033[34m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m"
+FOLDER="/input"
+
+iniciar_job(){
+    echo "${RED}${date '+%Y-%m-%d  %H:%M:%S'}${NC} | ${GREEN}INFO${NC} | Iniciando a pipeline de validação de transações..."
+
+}
+
+verificando_arquivo(){
+    echo "${RED}${date '+%Y-%m-%d  %H:%M:%S'}${NC} | ${BLUE}INFO${NC} | Verificando a existência do arquivo de transações..."
+    while true;
+    do
+        if ls "${FOLDER}/*.csv"
+}
+#python transactions_pipeline_main.py 
