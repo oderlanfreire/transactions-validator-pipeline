029b64ef79ff1e86df44cbadddbe637fad3d6aff
Oderlan Freire <oderlanfreire@gmail.com>
2025-12-04 09:45:58 -0300

    feature: ajuste inicial do script da main, adição de alguns imports e criação de função inicial

--- transactions_pipeline_main.py
+++ transactions_pipeline_main.py
@@ -1,1 +1,32 @@
+from config.setup_logging import setup_logging
+setup_logging(log_folder='./logs/pipeline')
+
 import pandas as pd
+import logging
+from modules.reader import read_file
+
+def read_transactions_file(file_path: str):
+    logging.info(f"Iniciando leitura do arquivo {file_path}")
+    dataframe = read_file(file_path)
+    logging.info(f"Arquivo {file_path} lido com sucesso, total de linhas: {len(dataframe)}")
+    return dataframe
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
