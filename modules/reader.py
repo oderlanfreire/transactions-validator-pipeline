286278e31a98d4d64ec86c915ca13a5ea3684c48
Oderlan Freire <oderlanfreire@gmail.com>
2025-12-04 09:44:52 -0300

    feature: script de reading criado

---
+++ modules/reader.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pandas.errors import EmptyDataError, ParserError
+
+
+def read_file(file_path: str) -> pd.DataFrame:
+    try:
+        dataframe = pd.read_csv(file_path)
+        return dataframe
+    except FileNotFoundError:
+        raise FileNotFoundError(f"O arquivo não foi encontrado na pasta: {file_path}")
+    except EmptyDataError:
+        raise EmptyDataError(f"O arquivo {file_path} está vazio.")
+    except ParserError as e:
+        raise ParserError(f"Formato de arquivo inválido: {e}")
+    except PermissionError:
+        raise PermissionError(f"Permissão negada ao tentar acessar: {file_path}")
+    except Exception as e:
+        raise RuntimeError(f"Falha ao ler o arquivo: {e}")
